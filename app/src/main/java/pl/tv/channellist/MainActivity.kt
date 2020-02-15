355888f33316c2d28df7d6c2adfb5808a24d4cb7
Adam_Z <youngtoss38@gmail.com>
2020-02-15 19:07:41 +0100

    Add fragment

--- app/src/main/java/pl/tv/channellist/MainActivity.kt
+++ app/src/main/java/pl/tv/channellist/MainActivity.kt
@@ -2,11 +2,29 @@ package pl.tv.channellist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottom_navigation_view.setOnNavigationItemSelectedListener {
+
+//            val selectedFragment:Fragment
+//
+//            when(it.itemId){
+//                R.id.nav_tv->
+//            }
+
+
+
+            true
+        }
     }
+
+
+
 }
