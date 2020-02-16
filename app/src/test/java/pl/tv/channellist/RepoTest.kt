7c659381b445e32e7d35d4e9f2c1d8556ab10314
Adam_Z <youngtoss38@gmail.com>
2020-02-16 22:49:26 +0100

    Added repoTest

---
+++ app/src/test/java/pl/tv/channellist/RepoTest.kt
@@ -0,0 +1,28 @@
+package pl.tv.channellist
+
+import org.junit.Test
+import junit.framework.TestCase.*
+import pl.tv.channellist.model.data.TvWebScrapper
+import pl.tv.channellist.model.repository.ProgrammeRepository
+
+class RepoTest {
+
+
+    val programmeRepository = ProgrammeRepository("https://www.cyfrowypolsat.pl/redir/program-tv/program-tv-pionowy-single-channel.cp?chN=",
+        listOf("tvn","polsat"), listOf(), TvWebScrapper("table","span",
+            "table.tableOgladaj","a.name"))
+
+    @Test
+    fun doFindProgrammesTest(){
+        val programmes = programmeRepository.doFindProgrammes()
+        assertTrue(programmes.isNotEmpty())
+
+        for(i in programmes){
+            println(i.name+","+i.movieList.joinToString(", "))
+        }
+
+
+    }
+
+
+}
