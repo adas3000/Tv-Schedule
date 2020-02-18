253847bfe94f831f7fc7108aa4a3d41daec5ce47
Adam_Z <youngtoss38@gmail.com>
2020-02-18 21:42:27 +0100

    Add LoadActivity

---
+++ app/src/main/java/pl/tv/channellist/view/ui/LoadActivity.kt
@@ -0,0 +1,13 @@
+package pl.tv.channellist.view.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import pl.tv.channellist.R
+
+class LoadActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_load)
+    }
+}
